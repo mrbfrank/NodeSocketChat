c2a27bcc23eae0f78accb883aca06c92302cb228
Brian Frank <mrbfrank@gmail.com>
2016-04-06 21:32:29 -0500

    bigfix deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var app = express();
-var server = app.listen(80);
+var server = app.listen(process.env.PORT || 1337);
 var io = require('socket.io').listen(server);
 
 // serve public assets
