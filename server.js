1feeee2a41407e0284f2ba4b47dab78e54cb3ed1
Brian Frank <mrbfrank@gmail.com>
2016-04-08 13:29:42 -0500

    convert indents --> 2 spaces

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.static('public'));
 // sockets
 io.sockets.on('connection', function(client) {
 
-	// when a message is received, emit to all clients.
-	client.on('message:client', function(data) {
-		client.broadcast.emit('message:server', {message: data.message});
-	});
+  // when a message is received, emit to all clients.
+  client.on('message:client', function(data) {
+    client.broadcast.emit('message:server', {message: data.message});
+  });
 });
